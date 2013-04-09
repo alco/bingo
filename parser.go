b72ecc4f877c4920465fe76b6dded5a040a224ac
Alexei Sholik <alcosholik@gmail.com>
2013-04-10 02:24:36 +0300

    Test basic types

--- parser.go
+++ parser.go
@@ -1,12 +1,40 @@
 package goblin
 
-import "io"
+import (
+    "encoding/binary"
+    "io"
+)
 
 type Parser struct {
     r io.Reader
+    byteOrder binary.ByteOrder
     offset uint32
 }
 
 func (p *Parser) EmitReadStruct(data interface{}) {
+    // Try fast path for fixed-size data
+    if p.EmitReadFixed(data) {
+        return
+    }
 
+    // TODO
+}
+
+func (p *Parser) EmitReadFixed(data interface{}) bool {
+	size := binary.Size(data)
+    if size < 0 {
+        return false
+    }
+
+	err := binary.Read(p.r, p.byteOrder, data)
+	if err != nil {
+        p.RaiseError(err)
+	}
+	p.offset += uint32(size)
+
+    return true
+}
+
+func (p *Parser) RaiseError(err error) {
+    panic(err)
 }
--- parser_test.go
+++ parser_test.go
@@ -3,12 +3,94 @@ package goblin
 import (
     "testing"
     "bytes"
+    "encoding/binary"
 )
 
-var someData = []byte{10, 0, 0, 0}
+var someData = []byte{10, 0, 1, 0}
+
+func newParser() *Parser {
+    return &Parser{bytes.NewReader(someData), binary.LittleEndian, 0}
+}
 
 func TestEmptyStruct(t *testing.T) {
     empty := struct {}{}
-    p := Parser{bytes.NewReader(someData), 0}
+    p := newParser()
+
     p.EmitReadStruct(&empty)
+
+    if p.offset != 0 {
+        t.Error("Non-zero offset after reading into empty struct")
+    }
+}
+
+func TestLength32Little(t *testing.T) {
+    s := struct {
+        Length uint32
+    }{}
+    p := newParser()
+    p.byteOrder = binary.LittleEndian
+
+    p.EmitReadStruct(&s)
+
+    if s.Length != 0x1000A {
+        t.Error("Failed to read a single-field struct of uint32 (little):", s.Length)
+    }
+
+    if p.offset != 4 {
+        t.Error("Invalid offset after reading uint32 (little):", p.offset)
+    }
+}
+
+func TestLength16Little(t *testing.T) {
+    s := struct {
+        Length uint16
+    }{}
+    p := newParser()
+    p.byteOrder = binary.LittleEndian
+
+    p.EmitReadStruct(&s)
+
+    if s.Length != 10 {
+        t.Error("Failed to read a single-field struct of uint16 (little):", s.Length)
+    }
+
+    if p.offset != 2 {
+        t.Error("Invalid offset after reading uint16 (little):", p.offset)
+    }
+}
+
+func TestLength32Big(t *testing.T) {
+    s := struct {
+        Length uint32
+    }{}
+    p := newParser()
+    p.byteOrder = binary.BigEndian
+
+    p.EmitReadStruct(&s)
+
+    if s.Length != 0x0A000100 {
+        t.Error("Failed to read a single-field struct of uint32 (big):", s.Length)
+    }
+
+    if p.offset != 4 {
+        t.Error("Invalid offset after reading uint32 (big):", p.offset)
+    }
+}
+
+func TestLength16Big(t *testing.T) {
+    s := struct {
+        Length uint16
+    }{}
+    p := newParser()
+    p.byteOrder = binary.BigEndian
+
+    p.EmitReadStruct(&s)
+
+    if s.Length != 0x0A00 {
+        t.Error("Failed to read a single-field struct of uint16 (big):", s.Length)
+    }
+
+    if p.offset != 2 {
+        t.Error("Invalid offset after reading uint16 (big):", p.offset)
+    }
 }
